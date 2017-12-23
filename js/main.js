f2a02be7d3742c999eb63b614282292b740562ef
frolov5 <i.slizh.garik@gmail.com>
2017-12-23 13:20:39 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+    $(document).on('change', '.tab-checked', function () {
+        var id = $(this).attr('value');
+        $('.tab-content-active').removeClass('tab-content-active').addClass('tab-content-hidden');
+        $('#'+id).addClass('tab-content-active');
+        return false;
+    });
+
+    $('.slider').slick({
+    	infinite: true,
+  		slidesToShow: 3,
+  		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 3000,
+
+    });
+
+    $('.slider-float').slick({
+    	infinite: true,
+  		slidesToShow: 5,
+  		slidesToScroll: 1,
+  		autoplay: true,
+  		autoplaySpeed: 2000,
+
+    });
+});
+
+$('.inline-block-questions').each(function () {
+var colors = ['green', 'pink', 'orange', 'Tan2', 'RosyBrown', 'Sienna2', 'MediumOrchid', 'Tomato', 'SandyBrown', 'DarkOrange', 'SeaGreen', 'DarkSlateGray', 'SlateGrey', 'MidnightBlue', 'CornflowerBlue', 'SlateBlue', 'RoyalBlue', 'SteelBlue'];
+var rand = Math.floor(Math.random() * colors.length); 
+$(this).css('border', '2px solid ' + colors[rand]);
+})
+
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $(window).scroll(function(){
+        var elem = $('.category-block');
+        if ($(this).scrollTop() > 300) {
+            elem.addClass('fixed-category-block');
+        }else {
+            elem.removeClass('fixed-category-block');
+        }
+    });
+
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
+});
+
+$(document).ready(function(){
+    $('#nav-icon').click(function(){
+        $(this).toggleClass('open');
+    });
+});
+
+$(document).ready(function() {
+    $("#select").select2({
+        placeholder: "Уровень образования",
+        allowClear: true
+    });
+});
+
+
