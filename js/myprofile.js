0d6d2738348618a64c629002ca7c200906257acc
frolov5 <i.slizh.garik@gmail.com>
2018-01-07 11:56:59 +0300

    стилизация и функционал.переделал первую вкладку профиля под новые требования руководства

--- js/myprofile.js
+++ js/myprofile.js
@@ -10,6 +10,12 @@ $(document).ready(function () {
         $('#' + id).addClass('tab-content-active');
         return false;
     });
+    $(".category-profile form").hide();
+    $(".form-active").show();
+    $("a.basic","a.education","a.work","a.skill","a.about_me").on("click", function () {
+        $(".category-profile form").hide();
+        $("#basic","#education","#work","#skill","#about_me").slideToggle();
+    })
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
             $('.scrollup').fadeIn();
