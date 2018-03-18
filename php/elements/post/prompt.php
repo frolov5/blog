360985af648b8ac0fa34316826c0ecb91a3f8cdd
frolov5 <i.slizh.garik@gmail.com>
2018-03-18 12:22:05 +0300

    стилизация.стилизировал блок-фильтр на странице мой профиль и добавил заливку для активного таба

---
+++ php/elements/post/prompt.php
@@ -0,0 +1,20 @@
+<div class="row prompt">
+    <form action="#" id="search-form" method="post">
+        <div class="col-sm-12 form-group search-and-button">
+            <input type="search" class="form-control search-by-word" name="search-by-word" placeholder="Поиск по слову...">
+            <input type="submit" class="btn btn-primary" value="Найти">
+        </div>
+        <div class="col-sm-6 select-category">
+            <select id="search">
+                <option>ИТ</option>
+                <option>Готовим дома</option>
+                <option>Гаражный ремонт авто</option>
+                <option>Идеи для интерьера</option>
+            </select>
+        </div>
+        <div class="col-sm-6 form-group input-date">
+            <input type="text" class="form-control" name="from-date" placeholder="Дата от">
+            <input type="text" class="form-control" name="to-date" placeholder="Дата до">
+        </div>
+    </form>
+</div>
--- php/elements/tabs/tab-records.php
+++ php/elements/tabs/tab-records.php
@@ -1,28 +1,9 @@
 <div id="content-tab-records" class="tab-content-hidden">
     <div class="col-sm-8">
         <div class="rg-profile profile-prompt">
-            <div class="row prompt">
-                <form action="#" id="search-form" method="post">
-                    <div class="col-sm-4 select-category">
-                        <select id="search">
-                            <option>ИТ</option>
-                            <option>Готовим дома</option>
-                            <option>Гаражный ремонт авто</option>
-                            <option>Идеи для интерьера</option>
-                        </select>
-                    </div>
-                    <div class="col-sm-4 form-group input-date">
-                        <input type="text" class="form-control" name="from-date" placeholder="Дата от">
-                        <input type="text" class="form-control" name="to-date" placeholder="Дата до">
-                    </div>
-                    <div class="col-sm-4 form-group">
-                        <input type="search" class="form-control search-by-word" name="search-by-word" placeholder="Поиск по слову...">
-                    </div>
-                    <div class="col-sm-offset-4 col-sm-4 button">
-                        <input type="button" class="btn btn-primary" value="Найти">
-                    </div>
-                </form>
-            </div>
+            <?php
+                require_once $_SERVER['DOCUMENT_ROOT'] . '/php/elements/post/prompt.php';
+            ?>
         </div>
     </div>
     <div class="col-sm-8 col-pad">
